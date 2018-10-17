b19b1590d2af1dbee45321239bca29b32cb4ec4f
Haliza Hayyu Firdausy <halizahayyu@gmail.com>
2018-10-17 07:40:06 +0700

    Belajar git pertama..

--- Testing/src/testing/Belajar1.java
+++ Testing/src/testing/Belajar1.java
@@ -9,14 +9,6 @@ package testing;
  *
  * @author haliz
  */
-public class mainTest {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-    }
-    
+public class Belajar1 {
+    System.out.println("Belajar");
 }
